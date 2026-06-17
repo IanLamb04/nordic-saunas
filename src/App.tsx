a967886afe21157dfb80b55161b9dd41e2dfb8cf
ianlamb04 <ianlamb2004@gmail.com>
2026-06-17 10:51:06 -0500

    Deleted existing content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Philosophy from "./components/Philosophy";
-import Materials from "./components/Materials";
-import Craftsmanship from "./components/Craftsmanship";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
 const App = () => (
   <>
-    <Navbar />
-    <main>
-      <Hero />
-      <Philosophy />
-      <Materials />
-      <Craftsmanship />
-      <Experience />
-      <Contact />
-    </main>
-    <Footer />
+    
   </>
 );
 
